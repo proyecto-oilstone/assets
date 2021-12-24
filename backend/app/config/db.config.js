4ef1a7398257d9a799b0ae307b13c5fcbab9a025
Tomas <tomasarras@gmail.com>
2021-12-23 21:19:13 -0300

    added differents configurations for production and development, frontend working

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: process.env.HOST,
+    HOST: process.env.POSTGRES_HOST,
     USER: process.env.POSTGRES_USER,
     PASSWORD: process.env.POSTGRES_PASSWORD,
     DB: process.env.POSTGRES_DB,
