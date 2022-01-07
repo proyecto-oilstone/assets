f67a7809c787fd4851bd065847afc421d4adfc90
Tomas <tomasarras@gmail.com>
2022-01-06 21:37:31 -0300

    added cars from context

--- frontend/src/components/CarList/CarList.js
+++ frontend/src/components/CarList/CarList.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import { Col, Row } from "react-bootstrap";
 import styles from "./CarList.module.css";
+import CarContext from "../../contexts/cars/CarContext";
 
 const CarList = () => {
-    const showEditCarModal = () => {
-        
-    };
+  const { cars } = useContext(CarContext);
+  
+  const showEditCarModal = () => {
+    
+  };
 
-    return (
-    <Row className='g-4'>
-        {cars.map(car => 
-            <Col key={car.id} sm="4">
-                <div className='bg-light rounded p-4 mx-2'>
-                    <div className='d-flex justify-content-between'>
-                        <span>{car.patente}</span>
-                        <img className={styles.editIcon} src="./icons/edit-solid.svg" alt="editar" onClick={showEditCarModal}/>
-                    </div>
-                    <span>{`${car.marca} ${car.modelo}`}</span>
-                </div>
-            </Col>
-        )}
-    </Row>
-    );
+  return (
+  <Row className='g-4'>
+    {cars.map(car => 
+      <Col key={car.id} sm="4">
+        <div className='bg-light rounded p-4 mx-2'>
+          <div className='d-flex justify-content-between'>
+            <span>{car.patente}</span>
+              <img className={styles.editIcon} src="./icons/edit-solid.svg" alt="editar" onClick={showEditCarModal}/>
+          </div>
+          <span>{`${car.marca} ${car.modelo}`}</span>
+        </div>
+      </Col>
+    )}
+  </Row>
+  );
 }
 
 export default CarList
