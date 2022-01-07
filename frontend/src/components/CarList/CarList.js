59042b16879a3035685ed0d66c78f93746272780
Tomas <tomasarras@gmail.com>
2022-01-06 21:33:07 -0300

    added component to list cars

---
+++ frontend/src/components/CarList/CarList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Col, Row } from "react-bootstrap";
+import styles from "./CarList.module.css";
+
+const CarList = () => {
+    const showEditCarModal = () => {
+        
+    };
+
+    return (
+    <Row className='g-4'>
+        {cars.map(car => 
+            <Col key={car.id} sm="4">
+                <div className='bg-light rounded p-4 mx-2'>
+                    <div className='d-flex justify-content-between'>
+                        <span>{car.patente}</span>
+                        <img className={styles.editIcon} src="./icons/edit-solid.svg" alt="editar" onClick={showEditCarModal}/>
+                    </div>
+                    <span>{`${car.marca} ${car.modelo}`}</span>
+                </div>
+            </Col>
+        )}
+    </Row>
+    );
+}
+
+export default CarList
