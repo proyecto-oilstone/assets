e9fbb10c7f2927564f6b37c5f9ee3cf1046b89c2
Tomas <tomasarras@gmail.com>
2022-01-06 21:28:12 -0300

    added modal to create provider

--- frontend/src/components/Modals/CreateProviderModal/CreateProviderModal.js
+++ frontend/src/components/Modals/CreateProviderModal/CreateProviderModal.js
@@ -6,29 +6,67 @@ import ProviderContext from "../../../contexts/providers/ProviderContext";
 const CreateProviderModal = (props) => {
   const { show, toggle } = props;
   const providerContext = useContext(ProviderContext);
-  const [name, setName] = useState("");
+  const [nombreCorto, setNombreCorto] = useState("");
+  const [nombreLargo, setNombreLargo] = useState("");
+  const [observaciones, setObservaciones] = useState("");
+
+  const resetFields = () => {
+    setNombreCorto("");
+    setNombreLargo("");
+    setObservaciones("");
+  }
 
   const handleOnClick = () => {
-    providerContext.createProvider({nombre: name});
-    setName("")
-    toggle()
+    providerContext.createProvider({nombreCorto, nombreLargo, observaciones});
+    toggle();
+    resetFields();
   }
 
   return (
     <CustomModal show={show} toggle={toggle} title="Crear proveedor">
       <Form>
         <Form.Group as={Row} className="mb-2">
-          <Form.Label column sm="12">
-            Nombre del proveedor
-          </Form.Label>
           <Col sm="6">
-            <Form.Control value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Ingresar el titulo" />
+            <Row>
+              <Form.Label column sm="12">
+                Nombre corto
+              </Form.Label>
+              <Col sm="12">
+                <Form.Control
+                  value={nombreCorto}
+                  onChange={(e) => setNombreCorto(e.target.value)}
+                  type="text"
+                  placeholder="Ingresa un nombre corto"
+                />
+              </Col>
+            </Row>
           </Col>
+
           <Col sm="6">
-            <Button onClick={handleOnClick}>Crear</Button>
+            <Row>
+              <Form.Label column sm="12">
+                Nombre largo
+              </Form.Label>
+              <Col sm="12">
+                <Form.Control
+                  value={nombreLargo}
+                  onChange={(e) => setNombreLargo(e.target.value)}
+                  type="text"
+                  placeholder="Ingresar un nombre largo"
+                />
+              </Col>
+            </Row>
           </Col>
         </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Observaciones</Form.Label>
+          <Form.Control value={observaciones} onChange={(e) => setObservaciones(e.target.value)} as="textarea" rows={3} />
+        </Form.Group>
       </Form>
+      <Col sm="6">
+        <Button onClick={handleOnClick}>Crear</Button>
+      </Col>
     </CustomModal>
   );
 };
