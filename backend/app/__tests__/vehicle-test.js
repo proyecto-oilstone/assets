e19df338b81946a97c634742e7df9384292d1431
LucasChaves12 <chaves.lucas2000@gmail.com>
2022-02-21 09:35:47 -0300

    Fixed Testing

--- backend/app/__tests__/vehicle-test.js
+++ backend/app/__tests__/vehicle-test.js
@@ -8,7 +8,7 @@ const app = require("../../server");
 
 const api = supertest(app);
 
-const { db } = require("../db/index");
+const { db, Provider, CarType } = require("../db/index");
 
 
 
@@ -27,8 +27,32 @@ describe("test-home", () => {
 describe("testing-vehicle-routes", () => {
     
     beforeAll(async () => {
-       await db.sequelize.sync({alter: true});
+       await db.sequelize.sync({force: true});
+       await Provider.create({
+            nombreLargo: "provider1",
+            nombreCorto: "provider1",
+            observaciones: "observaciones1",
+       })
+         await CarType.create({
+            nombreLargo: "carType1",
+            nombreCorto: "carType1",
+            observaciones: "observaciones1",
+         })
+
     })
+    it("POST /api/cars/autos ", async () => {
+        await api.post('/api/cars/nuevoAuto')
+        .send({
+            patente: "AAAA123",
+            año: 2013,
+            ProviderId: 1,
+            CarTypeId: 1,
+
+        })
+        .expect(200)
+        .expect('Content-Type', /json/)
+
+})
     it("GET /api/cars/autos", async () => {
         await api.get('/api/cars/autos')
         .expect(200)
@@ -36,37 +60,30 @@ describe("testing-vehicle-routes", () => {
 
         
     })
-    it("POST /api/cars/autos ", async () => {
-        await api.post('/api/cars/nuevoAuto')
-        .send({
-            patente: "AAAAA23111",
-            año: 20013,
-            ProviderId: 5,
-            CarTypeId: 8,
-
-        })
-        .expect(200)
-        .expect('Content-Type', /json/)
-
-})
 
     it("GET /api/cars/autos/:id", async () => {
-    await api.get('/api/cars/autos/20')
+    await api.get('/api/cars/autos/1')
     .expect(200)
     .expect('Content-Type', /json/)
     
 })
     it("PUT /api/cars/editAuto/:id", async () => {
-    await api.put('/api/cars/editAuto/20')
+    await api.put('/api/cars/editAuto/1')
     .send({
         patente: "ABDASD231",
         año: 1999,
-        ProviderId: 5,
-        CarTypeId: 8,
+        ProviderId: 1,
+        CarTypeId: 1,
 
     })
     .expect(200)
     .expect('Content-Type', /json/)
 })
+    it("DELETE /api/cars/autos/:id", async () => {
+        await api.delete('/api/cars/autos/1')
+        .expect(200)
+        .expect('Content-Type', /json/)
+
+    })
 
 })
